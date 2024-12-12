3912137b1360f1cc5f4007268e2cce9cb9013d9a
pabroux <pierre-alexandrebroux@live.fr>
2024-12-12 11:18:32 +0100

    feat: handle pipe input for home.activation using 'read'.

--- module/homebrew/app/raycast/config.nix
+++ module/homebrew/app/raycast/config.nix
@@ -1,5 +1,7 @@
 "echo -n 'Do you want to import raycast config? [y/n]: '
 read -r REPLY
+[ ! -t 0 ] && echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 	/usr/bin/open ${toString (./. + "/raycast.rayconfig")}
-fi"
+fi
+"
--- module/yabai/default.nix
+++ module/yabai/default.nix
@@ -15,6 +15,7 @@
 		setupYabaiScriptingAddition = lib.hm.dag.entryAfter ["writeBoundary"] 
 		("echo -n 'Do you want to setup the yabai scripting addition (superuser privileges required)? [y/n]: '
 		read -r REPLY
+		[ ! -t 0 ] && echo
 		if [[ $REPLY =~ ^[Yy]$ ]]; then
 			echo \"$(/usr/bin/whoami) ALL=(root) NOPASSWD: sha256:$(/usr/bin/shasum -a 256 $(/bin/realpath $HOME)/.nix-profile/bin/yabai | /usr/bin/cut -d ' ' -f 1) $(/bin/realpath $HOME)/.nix-profile/bin/yabai --load-sa\" | /usr/bin/sudo /usr/bin/tee /private/etc/sudoers.d/yabai > /dev/null
 		fi
