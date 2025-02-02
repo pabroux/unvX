26c78080f1c73971e74113fdad3a59c410cffc92
pabroux <pierre-alexandrebroux@live.fr>
2025-02-02 12:23:30 +0100

    feat(neovim): change smart-splits key bindings.

--- module/neovim/plugins/specs/smart-splits.lua
+++ module/neovim/plugins/specs/smart-splits.lua
@@ -1,21 +1,24 @@
 return {
 	"mrjones2014/smart-splits.nvim",
 	config = function()
+		local smart_splits = require("smart-splits")
+
 		-- Key bindings
 		-- ↳ Resize pane (the following keymaps accept a range)
-		vim.keymap.set("n", "<C-h>", require("smart-splits").resize_left)
-		vim.keymap.set("n", "<C-j>", require("smart-splits").resize_down)
-		vim.keymap.set("n", "<C-k>", require("smart-splits").resize_up)
-		vim.keymap.set("n", "<C-l>", require("smart-splits").resize_right)
-		-- ↳ Focus pane
-		vim.keymap.set("n", "Ì", require("smart-splits").move_cursor_left)
-		vim.keymap.set("n", "Ï", require("smart-splits").move_cursor_down)
-		vim.keymap.set("n", "È", require("smart-splits").move_cursor_up)
-		vim.keymap.set("n", "¬", require("smart-splits").move_cursor_right)
+		vim.keymap.set("n", "<C-h>", smart_splits.resize_left)
+		vim.keymap.set("n", "<C-j>", smart_splits.resize_down)
+		vim.keymap.set("n", "<C-k>", smart_splits.resize_up)
+		vim.keymap.set("n", "<C-l>", smart_splits.resize_right)
+		-- ↳ Focus pane (the following keymaps accept a range)
+		vim.keymap.set("n", "Ì", smart_splits.move_cursor_left)
+		vim.keymap.set("n", "Ï", smart_splits.move_cursor_down)
+		vim.keymap.set("n", "È", smart_splits.move_cursor_up)
+		vim.keymap.set("n", "¬", smart_splits.move_cursor_right)
 
 		-- Options
-		require("smart-splits").setup({
+		smart_splits.setup({
 			at_edge = "stop",
+			multiplexer_integration = false,
 		})
 	end,
 }
