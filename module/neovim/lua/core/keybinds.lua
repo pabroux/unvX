a4422b0e5b4874c2135e87fe0c3530ccab7ba3da
pabroux <pierre-alexandrebroux@live.fr>
2025-02-15 00:51:04 +0100

    style(neovim): change keybind descriptions.

--- module/neovim/lua/core/keybinds.lua
+++ module/neovim/lua/core/keybinds.lua
@@ -5,20 +5,20 @@ vim.g.mapleader = " "
 
 -- Normal mode
 -- ↳ Clear search highlights
-keymap.set("n", "<leader>nh", "<cmd>nohl<CR>", { desc = "Clear search highlights." })
--- ↳ Explorer
-keymap.set("n", "<leader>ex", "<cmd>Explore<CR>", { desc = "Clear search highlights." })
+keymap.set("n", "<leader>nh", "<cmd>nohl<CR>", { desc = "Clear search highlights" })
 -- ↳ Increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number." })
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number." })
--- ↳ Window management
-keymap.set("n", "<leader>d", "<C-w>v", { desc = "Split window vertically." })
-keymap.set("n", "<leader>D", "<C-w>s", { desc = "Split window horizontally." })
-keymap.set("n", "<leader>=", "<C-w>=", { desc = "Make splits equal size." })
-keymap.set("n", "<leader>w", "<cmd>close<CR>", { desc = "Close current split." })
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+-- ↳ Navigation
+keymap.set("n", "<C-$>", "<C-]>", { desc = "Navigate forward" })
 -- ↳ Tab management
-keymap.set("n", "<leader>t", "<cmd>tabnew<CR>", { desc = "Open new tab." })
-keymap.set("n", "<leader>W", "<cmd>tabclose<CR>", { desc = "Close current tab." })
-keymap.set("n", "<leader>l", "<cmd>tabn<CR>", { desc = "Go to next tab." })
-keymap.set("n", "<leader>h", "<cmd>tabp<CR>", { desc = "Go to previous tab." })
-keymap.set("n", "<leader>T", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab." })
+keymap.set("n", "<leader>t", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>W", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>l", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>h", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>T", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+-- ↳ Window management
+keymap.set("n", "<leader>d", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>D", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>=", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>w", "<cmd>close<CR>", { desc = "Close current split" })
