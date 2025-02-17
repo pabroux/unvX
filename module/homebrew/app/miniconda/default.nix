1a4b0473c09224139d071ca1e31eaaa88fb55a9c
pabroux <pierre-alexandrebroux@live.fr>
2025-02-17 16:11:28 +0100

    style(module): use better indent for shell scripts.

--- module/homebrew/app/miniconda/default.nix
+++ module/homebrew/app/miniconda/default.nix
@@ -8,14 +8,14 @@
     setupMiniconda =
       lib.hm.dag.entryAfter ["installHomebrewCasks"]
       "(
-				PATH=/opt/homebrew/bin:$PATH
-				if [[ -v DRY_RUN ]]; then
-					echo 'conda config --set auto_activate_base false'
-					echo 'conda config --set changeps1 false'
-				else
-					conda config --set auto_activate_base false
-					conda config --set changeps1 false
-				fi
-			 )";
+        PATH=/opt/homebrew/bin:$PATH
+        if [[ -v DRY_RUN ]]; then
+          echo 'conda config --set auto_activate_base false'
+          echo 'conda config --set changeps1 false'
+        else
+          conda config --set auto_activate_base false
+          conda config --set changeps1 false
+        fi
+      )";
   };
 }
--- module/homebrew/app/raycast/default.nix
+++ module/homebrew/app/raycast/default.nix
@@ -8,17 +8,17 @@
     setupRaycast =
       lib.hm.dag.entryAfter ["installHomebrewCasks"]
       "(
-				PATH=/usr/bin:$PATH
-		  		echo -n 'Do you want to import raycast config? [y/n]: '
-		  		read -r reply
-		  		[ ! -t 0 ] && echo
-		  		if [[ $reply =~ ^[Yy]$ ]]; then
-						if [[ -v DRY_RUN ]]; then
-		  				echo 'open ${toString (./. + "/raycast.rayconfig")}'
-						else
-							open ${toString (./. + "/raycast.rayconfig")}
-						fi
-		  		fi
-			 )";
+        PATH=/usr/bin:$PATH
+        echo -n 'Do you want to import raycast config? [y/n]: '
+        read -r reply
+        [ ! -t 0 ] && echo
+        if [[ $reply =~ ^[Yy]$ ]]; then
+          if [[ -v DRY_RUN ]]; then
+            echo 'open ${toString (./. + "/raycast.rayconfig")}'
+          else
+            open ${toString (./. + "/raycast.rayconfig")}
+          fi
+        fi
+      )";
   };
 }
