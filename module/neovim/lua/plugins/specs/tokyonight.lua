4bc916f42f64a51fb06b92950686e235a91e682e
pabroux <pierre-alexandrebroux@live.fr>
2025-02-23 01:15:16 +0100

    feat(neovim): change color scheme.

--- module/neovim/lua/plugins/specs/tokyonight.lua
+++ module/neovim/lua/plugins/specs/tokyonight.lua
@@ -11,11 +11,11 @@ return {
 		local bg = "#011628"
 		local bg_dark = "#000f1c"
 		local bg_highlight = "#143652"
-		local bg_search = "#0A64AC"
+		local bg_search = "#0a64ac"
 		local bg_visual = "#275378"
-		local fg = "#CBE0F0"
-		local fg_dark = "#B4D0E9"
-		local fg_gutter = "#627E97"
+		local fg = "#cbe0f0"
+		local fg_dark = "#b4d0e9"
+		local fg_gutter = "#627e97"
 		local border = "#000f1c"
 
 		tokyonight.setup({
