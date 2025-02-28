e3a9f2d127a5bc2b21ecf64ff7f8a68d31444338
pabroux <pierre-alexandrebroux@live.fr>
2025-02-28 13:28:01 +0100

    feat(neovim): add 'notifier' to 'snacks'.

--- module/neovim/lua/plugins/specs/snacks.lua
+++ module/neovim/lua/plugins/specs/snacks.lua
@@ -1,12 +1,36 @@
 return {
 	"folke/snacks.nvim",
 	priority = 1000,
+	lazy = false,
+	keys = {
+		-------------------------------------------------------------------------------
+		-- Key bindings
+		-------------------------------------------------------------------------------
+
+		{
+			"<leader>nd",
+			function()
+				Snacks.notifier.hide()
+			end,
+			desc = "Dismiss All Notifications",
+		},
+		{
+			"<leader>ns",
+			function()
+				Snacks.notifier.show_history()
+			end,
+			desc = "Notification History",
+		},
+	},
 	opts = {
 		-------------------------------------------------------------------------------
 		-- Options
 		-------------------------------------------------------------------------------
 
 		input = {
+			enabled = true,
+		},
+		notifier = {
 			enabled = true,
 		},
 	},
