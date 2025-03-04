723af1810e5e945dd10bbf3c5b461bc2d2d2fe5e
pabroux <pierre-alexandrebroux@live.fr>
2025-03-04 22:28:05 +0100

    feat(lazygit): define a new color scheme.

--- module/lazygit/default.nix
+++ module/lazygit/default.nix
@@ -1,9 +1,17 @@
 {
   config,
   pkgs,
+  unvX,
   ...
 }: {
   home.packages = [
     pkgs.lazygit
   ];
+
+  home.file = {
+    "lazygit/config.yml" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${unvX.directory.module}/lazygit/config.yml";
+      target = ".config/lazygit/config.yml";
+    };
+  };
 }
