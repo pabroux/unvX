def1d6f0ef861a50c2badd3f070d3efb6226bec1
pabroux <pierre-alexandrebroux@live.fr>
2025-03-04 16:03:55 +0100

    feat(neovim): change keybindings for tab switching.

--- module/neovim/lua/core/keybinds.lua
+++ module/neovim/lua/core/keybinds.lua
@@ -14,8 +14,8 @@ keymap.set("n", "<C-$>", "<C-]>", { desc = "Navigate forward" })
 -- ↳ Tab management
 keymap.set("n", "<leader>t", "<cmd>tabnew<cr>", { desc = "Open new tab" })
 keymap.set("n", "<leader>W", "<cmd>tabclose<cr>", { desc = "Close current tab" })
-keymap.set("n", "<leader>l", "<cmd>tabn<cr>", { desc = "Next tab" })
-keymap.set("n", "<leader>h", "<cmd>tabp<cr>", { desc = "Prev tab" })
+keymap.set("n", "<leader>é", "<cmd>tabn<cr>", { desc = "Next tab" })
+keymap.set("n", "<leader>&", "<cmd>tabp<cr>", { desc = "Prev tab" })
 keymap.set("n", "<leader>T", "<cmd>tabnew %<cr>", { desc = "Open current buffer in new tab" })
 -- ↳ Window management
 keymap.set("n", "<leader>d", "<C-w>v", { desc = "Split window vertically" })
