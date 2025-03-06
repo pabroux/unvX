34f43378ba83c8a659ab99e1ebab1a5cf1d6023e
pabroux <pierre-alexandrebroux@live.fr>
2025-03-06 16:05:24 +0100

    chore(neovim): change folder structure.

--- module/neovim/lua/core/keybinds.lua
+++ module/neovim/lua/core/keybinds.lua
@@ -7,10 +7,10 @@ vim.g.mapleader = " "
 -- ↳ Clear search highlights
 keymap.set("n", "<leader>nh", "<cmd>nohl<cr>", { desc = "Clear search highlights" })
 -- ↳ Increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+keymap.set("n", "<leader>+", "<c-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<c-x>", { desc = "Decrement number" })
 -- ↳ Navigation
-keymap.set("n", "<C-$>", "<C-]>", { desc = "Navigate forward" })
+keymap.set("n", "<c-$>", "<c-]>", { desc = "Navigate forward" })
 -- ↳ Tab management
 keymap.set("n", "<leader>t", "<cmd>tabnew<cr>", { desc = "Open new tab" })
 keymap.set("n", "<leader>W", "<cmd>tabclose<cr>", { desc = "Close current tab" })
@@ -18,7 +18,11 @@ keymap.set("n", "<leader>é", "<cmd>tabn<cr>", { desc = "Next tab" })
 keymap.set("n", "<leader>&", "<cmd>tabp<cr>", { desc = "Prev tab" })
 keymap.set("n", "<leader>T", "<cmd>tabnew %<cr>", { desc = "Open current buffer in new tab" })
 -- ↳ Window management
-keymap.set("n", "<leader>d", "<C-w>v", { desc = "Split window vertically" })
-keymap.set("n", "<leader>D", "<C-w>s", { desc = "Split window horizontally" })
-keymap.set("n", "<C-=>", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>d", "<c-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>D", "<c-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<c-=>", "<c-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>w", "<cmd>close<cr>", { desc = "Close current split" })
+
+-- Terminal mode
+-- ↳ Exit terminal mode
+keymap.set("t", "<esc>", [[<c-\><c-n>]], { desc = "Escape terminal mode" })
--- module/neovim/lua/core/lazy.lua
+++ module/neovim/lua/core/lazy.lua
@@ -18,7 +18,7 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
-		{ import = "plugins.specs" },
+		{ import = "plugins" },
 	},
 	checker = {
 		enabled = true,
