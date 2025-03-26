26c3f5090fa8c33bc59a77025e1a7bb9b370a183
pabroux <pierre-alexandrebroux@live.fr>
2025-03-26 10:45:29 +0100

    feat(neovim): add build step to 'mason'.

--- module/neovim/lua/plugins/mason.lua
+++ module/neovim/lua/plugins/mason.lua
@@ -3,6 +3,7 @@ return {
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
 	},
+	build = ":MasonUpdate",
 	config = function()
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
