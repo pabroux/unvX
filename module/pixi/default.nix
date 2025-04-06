c2eccf6f97d8398c922cb0b1f0e70e9169829eed
pabroux <pierre-alexandrebroux@live.fr>
2025-04-06 16:02:46 +0200

    feat(pixi): deactivate ps1 prompt modification.

--- module/pixi/default.nix
+++ module/pixi/default.nix
@@ -1,9 +1,23 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   home.packages = [
     pkgs.pixi
   ];
+
+  home.activation = {
+    setupPixi =
+      lib.hm.dag.entryAfter ["installPackages"]
+      "(
+        PATH=/opt/homebrew/bin:$PATH
+        if [[ -v DRY_RUN ]]; then
+          echo 'pixi config set change-ps1 false'
+        else
+          pixi config set change-ps1 false
+        fi
+      )";
+  };
 }
