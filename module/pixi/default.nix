96e1edae72b972b3ca8829a45da73115bb792481
pabroux <pierre-alexandrebroux@live.fr>
2025-04-06 16:44:01 +0200

    fix(pixi): fix ps1 prompt deactivation.

--- module/pixi/default.nix
+++ module/pixi/default.nix
@@ -12,11 +12,13 @@
     setupPixi =
       lib.hm.dag.entryAfter ["installPackages"]
       "(
-        PATH=/opt/homebrew/bin:$PATH
+        PATH=$HOME/.nix-profile/bin:/usr/bin:/bin:$PATH
         if [[ -v DRY_RUN ]]; then
           echo 'pixi config set shell.change-ps1 false'
-        else
+        elif [[ -v VERBOSE ]]; then
           pixi config set shell.change-ps1 false
+        else
+          pixi config set shell.change-ps1 false &> /dev/null
         fi
       )";
   };
