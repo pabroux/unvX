46d38b18925c1c01c1eab0c47ea3dec0dd4a6c51
pabroux <pierre-alexandrebroux@live.fr>
2025-05-04 11:50:53 +0200

    refactor(host): separate modules to use from the config.

--- host/Sulfyderz-MacBook-Pro/user/sulfyderz/home.nix
+++ host/Sulfyderz-MacBook-Pro/user/sulfyderz/home.nix
@@ -1,23 +1,10 @@
 {
   config,
   pkgs,
+  lib,
   ...
-}: {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "sulfyderz";
-  home.homeDirectory = "/Users/sulfyderz";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-
-  imports = map (x: ../../../../module + "/${x}") [
+}: let
+  modules = [
     "coreutils"
     "deno"
     "fzf"
@@ -44,6 +31,22 @@
     "zsh"
     "zsh-autosuggestions"
   ];
+in {
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "sulfyderz";
+  home.homeDirectory = "/Users/sulfyderz";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  imports = map (x: ../../../../module + "/${x}") modules;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
