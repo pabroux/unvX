39cbe39ff277035d7863ccc134a4f5139635ae76
pabroux <pierre-alexandrebroux@live.fr>
2025-06-13 10:29:43 +0200

    feat(neovim): remove linter from `nvim-lint`.

--- module/neovim/lua/plugins/nvim-lint.lua
+++ module/neovim/lua/plugins/nvim-lint.lua
@@ -21,7 +21,6 @@ return {
 			javascript = { "eslint_d" },
 			javascriptreact = { "eslint_d" },
 			python = { "pylint" },
-			svelte = { "eslint_d" },
 			typescript = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 		}
