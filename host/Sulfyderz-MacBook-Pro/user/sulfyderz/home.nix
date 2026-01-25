4f5699a924ae3f0acc8379ac6783b9e6639d3a97
pabroux <pierre-alexandrebroux@live.fr>
2026-01-25 19:00:14 +0100

    refactor(host): remove unfree predicate. [no ci]

--- host/Sulfyderz-MacBook-Pro/user/sulfyderz/home.nix
+++ host/Sulfyderz-MacBook-Pro/user/sulfyderz/home.nix
@@ -50,8 +50,6 @@ in {
 
   imports = map (x: ../../../../module + "/${x}") modules;
 
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) modules;
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
