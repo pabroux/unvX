55ede7389b286b07e504acd47eb9d0aee19bb615
pabroux <pierre-alexandrebroux@live.fr>
2026-02-08 00:38:58 +0100

    fix(neovim): update plugins.

--- module/neovim/lua/plugins/nvim-lspconfig.lua
+++ module/neovim/lua/plugins/nvim-lspconfig.lua
@@ -4,13 +4,10 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
-		{ "folke/neodev.nvim", opts = {} },
 	},
 	config = function()
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 		local keymap = vim.keymap
-		local lspconfig = require("lspconfig")
-		local mason_lspconfig = require("mason-lspconfig")
 
 		-------------------------------------------------------------------------------
 		-- Key bindings
@@ -54,13 +51,7 @@ return {
 		end
 
 		function toggle_diagnostics()
-			if vim.g.diagnostics_active then
-				vim.g.diagnostics_active = false
-				vim.diagnostic.enable()
-			else
-				vim.g.diagnostics_active = true
-				vim.diagnostic.disable()
-			end
+			vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 		end
 
 		keymap.del("n", "[d")
@@ -85,13 +76,22 @@ return {
 		-- Used to enable auto-completion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		vim.lsp.config("*", {
+			capabilities = capabilities,
+		})
 
-		mason_lspconfig.setup()
+		-- Change the Diagnostic symbols in the sign column (gutter)
+		local severity = vim.diagnostic.severity
+
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[severity.ERROR] = " ",
+					[severity.WARN] = " ",
+					[severity.HINT] = " ",
+					[severity.INFO] = " ",
+				},
+			},
+		})
 	end,
 }
