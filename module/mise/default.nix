5379729b7f3fa2196830da5c69da631db62153a5
pabroux <pierre-alexandrebroux@live.fr>
2026-02-08 00:42:01 +0100

    feat(mise): add config file.

--- module/mise/default.nix
+++ module/mise/default.nix
@@ -1,9 +1,17 @@
 {
   config,
   pkgs,
+  unvX,
   ...
 }: {
   home.packages = [
     pkgs.mise
   ];
+
+  home.file = {
+    "mise/config.toml" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${unvX.directory.module}/mise/config.toml";
+      target = ".config/mise/config.toml";
+    };
+  };
 }
