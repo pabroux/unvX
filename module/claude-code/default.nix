7494add722d103854f0acebd0cf4c51ff27ce069
pabroux <pierre-alexandrebroux@live.fr>
2026-02-15 14:56:39 +0100

    feat(claude): add agents.

--- module/claude-code/default.nix
+++ module/claude-code/default.nix
@@ -4,15 +4,30 @@
   lib,
   unvX,
   ...
-}: {
+}: let
+  agents = [
+    "DocsExplorer"
+  ];
+in {
   home.packages = [
     pkgs.claude-code
   ];
 
-  home.file = {
-    "claude/settings.json" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${unvX.directory.module}/claude-code/settings.json";
-      target = ".config/claude/settings.json";
-    };
-  };
+  home.file =
+    {
+      "claude/settings.json" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${unvX.directory.module}/claude-code/settings.json";
+        target = ".config/claude/settings.json";
+      };
+    }
+    // builtins.listToAttrs (map (agent: {
+        name = "claude/agents/${agent}.md";
+        value =
+          (agent: {
+            source = config.lib.file.mkOutOfStoreSymlink "${unvX.directory.module}/claude-code/agents/${agent}.md";
+            target = ".config/claude/agents/${agent}.md";
+          })
+          agent;
+      })
+      agents);
 }
