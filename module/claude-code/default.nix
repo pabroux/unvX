3668aec4b527ef05bb63d98c9a94237f4af9bf78
pabroux <pierre-alexandrebroux@live.fr>
2026-02-15 22:40:55 +0100

    feat(claude): add commands.

--- module/claude-code/default.nix
+++ module/claude-code/default.nix
@@ -23,5 +23,13 @@
           target = ".config/claude/agents/${agent}";
         };
       })
-      (builtins.attrNames (builtins.readDir ./agents)));
+      (builtins.attrNames (builtins.readDir ./agents)))
+    // builtins.listToAttrs (map (command: {
+        name = "claude/commands/${command}";
+        value = {
+          source = config.lib.file.mkOutOfStoreSymlink "${unvX.directory.module}/claude-code/commands/${command}";
+          target = ".config/claude/commands/${command}";
+        };
+      })
+      (builtins.attrNames (builtins.readDir ./commands)));
 }
