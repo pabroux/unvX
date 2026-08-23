f126a2ea1c9808dc0ec1159f98628d25cddfb568
pabroux <pierre-alexandrebroux@live.fr>
2026-08-23 23:36:36 +0200

    feat: make prompt for input loop until it's valid

--- module/brew/app/raycast/default.nix
+++ module/brew/app/raycast/default.nix
@@ -9,9 +9,19 @@
       lib.hm.dag.entryAfter ["installHomebrewCasks"]
       "(
         PATH=/usr/bin:$PATH
-        echo -n 'Do you want to import raycast config? [y/N]: '
-        read -r reply
-        [ ! -t 0 ] && echo
+        reply=n
+        while :; do
+          echo -n 'Do you want to import raycast config? [y/N]: '
+          if ! read -r reply; then
+            [ ! -t 0 ] && echo
+            reply=n
+            break
+          fi
+          if [[ -z $reply || $reply =~ ^[YyNn]$ ]]; then
+            break
+          fi
+          echo \"Please answer 'y' or 'n' (or press Enter for the default, n).\"
+        done
         if [[ $reply =~ ^[Yy]$ ]]; then
           if [[ -v DRY_RUN ]]; then
             echo 'open ${toString (./. + "/raycast.rayconfig")}'
--- module/yabai/default.nix
+++ module/yabai/default.nix
@@ -21,9 +21,19 @@
       lib.hm.dag.entryAfter ["installPackages"]
       "(
         PATH=$HOME/.nix-profile/bin:/usr/bin:/bin:$PATH
-        echo -n 'Do you want to setup the yabai scripting addition (superuser privileges required)? [y/N]: '
-        read -r reply
-        [ ! -t 0 ] && echo
+        reply=n
+        while :; do
+          echo -n 'Do you want to setup the yabai scripting addition (superuser privileges required)? [y/N]: '
+          if ! read -r reply; then
+            [ ! -t 0 ] && echo
+            reply=n
+            break
+          fi
+          if [[ -z $reply || $reply =~ ^[YyNn]$ ]]; then
+            break
+          fi
+          echo \"Please answer 'y' or 'n' (or press Enter for the default, n).\"
+        done
         if [[ $reply =~ ^[Yy]$ ]]; then
           if [[ -v DRY_RUN ]]; then
             echo 'echo \"$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d ' ' -f 1) $(which yabai) --load-sa\" | sudo tee /private/etc/sudoers.d/yabai > /dev/null'
